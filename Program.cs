06a4084762b02705ad3d57f1bbdb2ac6d56a6cd4
Koren Itay <Itay.Koren@motorolasolutions.com>
2019-10-04 23:55:07 +0300

    Itay Test

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace TodoApi
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            // Test - add change
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
