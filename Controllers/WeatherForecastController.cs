3528086e8134106ce89245efcf5b80e88d9c7183
Koren Itay <Itay.Koren@motorolasolutions.com>
2019-10-05 00:19:00 +0300

    Dockerfile

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace TodoApi.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            Console.WriteLine(nameof(WeatherForecast));
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
